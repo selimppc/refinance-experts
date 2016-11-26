cac26f3411ea2b357f9793c2e81d2170e8602a32
shimul <shimuldas72@gmail.com>
2016-11-26 23:55:08 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,325 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Refinance Experts</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/custom.css">
+	</head>
+	<body>
+		<header class="header">
+			<div class="container-fluid	">
+				<div class="row">
+					<!-- <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<a href="#"> -->
+							<img class='img-responsive' src="images/header-158-x-1600.jpg" >
+						<!-- </a>
+					</div> -->
+				</div>
+			</div>
+		</header>
+
+		<section class="banner">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-8 main-section">
+						<h2>Lowest Interest Ever</h3>
+						<div class="interest-rate">
+							
+							<div class="interest-rate-bg-image">
+
+								<div class="one ">3</div>
+								<div class="one ">9</div>
+								<div class="one two">2</div>
+														
+							</div>
+
+						</div>
+
+						<div class="offersection">
+							<h3>Costing you less</h3>
+							<ul>
+								<li>
+									<a href="#">No application fee</a>
+								</li>
+								<li>
+									<a href="#">No valuation fee</a>
+								</li>
+								<li>
+									<a href="#">No monthly or annual fee</a>
+								</li>
+								<li>
+									<a href="#">Free initial consulation</a>
+								</li>
+								<li>
+									<a href="#">We come to you</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="col-sm-4 sign-up">
+						<h3><em>SIGN UP</em> FOR A <strong>FREE CONSULTATION<sup>**</sup></strong></h3>
+						<p>If your home loan rate doesn't start with a three, then it's
+						time to find a better deal.</p>
+						<div class="form-group">
+							<label>First Name</label>
+							<input class="form-control" type="text">							
+						</div>
+						<div class="form-group">
+							<label>Last Name</label>
+							<input class="form-control" type="text">							
+						</div>
+						<div class="form-group">
+							<label>Email</label>
+							<input class="form-control" type="text">							
+						</div>
+						<div class="form-group">
+							<label>Phone Number</label>
+							<input class="form-control" type="text">							
+						</div>
+						<div class="form-group">
+							<label>Post Code</label>
+							<input class="form-control" type="text">							
+						</div>
+						<div style="float: left;height: auto;margin-top:5px;" class="form-group form-control no-border">							
+							<input style="float:left;width:10%;" type="checkbox">							
+							<label style="float:left;width:90%;">I would like to receive news and updates from Yellow Bricks Road.</label>
+						</div>
+						<div class="form-group">
+							<input style="background: #ee591f;border: 1px solid #ee591f;color: #fff;font-size: 17px;height: auto;margin-top: 5px;float: left;" class="form-control" type="submit" name="" value="Let's talk">							
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</section>
+
+		<section class="intro time_to_switch">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3 style="margin-bottom: 18px;" class="h3-header">It's time to switch</h3>
+						<p class="time_to_switch_p">Big banks are upping their rates, while ours are droping.
+							If your home loan rate doesn't start with a three, then it 
+							is time to find a better deal.</p>
+						<p class="time_to_switch_p">By switching to our new 3.92% comparisan rate for the Rate Smasher Home 
+							Loan you could save your family tens of thousands of dollars over
+							the life of your loan</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+		<section class="intro loan_compare_wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3 style="text-align:center;">How does Yellow Brick Road's Smasher home loan compare? </h3>
+					</div>
+
+					<div class="col-md-2">
+						<div class="loan_compare">
+							<img src="images/loan_compare6.png">
+							<div class="loan_compare_text" style="">
+								<p>
+									Borrower rate<br/>Comparison rate
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-2">
+						<div class="loan_compare">
+							<img src="images/loan_compare1.png">
+							<div class="loan_compare_text">
+								<p>
+									3.91% p.a<br/>3.92 p.a*
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-2">
+						<div class="loan_compare">
+							<img src="images/loan_compare2.png">
+							<div class="loan_compare_text">
+								<p>
+									4.62% p.a<br/>4.63 p.a*
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-2">
+						<div class="loan_compare">
+							<img src="images/loan_compare3.png">
+							<div class="loan_compare_text">
+								<p>
+									4.75% p.a<br/>4.75 p.a*
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-2">
+						<div class="loan_compare">
+							<img src="images/loan_compare4.png">
+							<div class="loan_compare_text">
+								<p>
+									4.15% p.a<br/>4.15 p.a*
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-2">
+						<div class="loan_compare">
+							<img src="images/loan_compare5.png">
+							<div class="loan_compare_text">
+								<p>
+									4.19% p.a<br/>4.2 p.a*
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="intro loan_rate" style="margin-top:50px;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h3 style="text-align:center;">
+							Why choose our Rate Smasher Home Loan
+						</h3>
+					</div>
+
+					<div class="col-md-6 item_cont">
+						<div class="rate_smasher_container">
+							<div class="images">
+								<img src="images/smasher1.png">
+							</div>
+							<div class="text">
+								<h4>Lowest rate over</h4>
+								<p>
+									As rates have lowered, Yellow Brick Road's 
+									gone even lower and introduced a new 3.92% p.a.
+									comparisan rate* for the Rate Smasher Home Loan.
+								</p>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="col-md-6 item_cont">
+						<div class="rate_smasher_container">
+							<div class="images">
+								<img src="images/smasher2.png">
+							</div>
+							<div class="text">
+								<h4>Local team, local access</h4>
+								<p>
+									Blacktown, Quaker Hils, Seven Hils etc.
+								</p>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="col-md-6 item_cont">
+						<div class="rate_smasher_container">
+							<div class="images">
+								<img src="images/smasher3.png">
+							</div>
+							<div class="text">
+								<h4>You never know if you never go</h4>
+								<p>
+									When you register your details to talk to an
+									adviser a home loan specialist will contact you 
+									for an obligation free* consultation to assess
+									your financial situation.
+								</p>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="col-md-6 item_cont">
+						<div class="rate_smasher_container">
+							<div class="images">
+								<img src="images/smasher4.png">
+							</div>
+							<div class="text">
+								<h4>Flexible payment options</h4>
+								<p>
+									Own your home sooner with unlimited additional 
+									repayments permitted on variable rate accounts.
+									Redraw is also avaliable on all variable rate
+									accounts.
+								</p>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="intro testimonial">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="testimonial_container">
+							<div class="leftside">
+								<p>
+									" People need quality financial advice, especially in diverse and growing communities like Colebee, Blacktown and they deserve to have a person that they trust to deliver those things. This is foundation, principle and inspiration behind Yellow Brick Road.
+								</p>
+								<p>
+									Jay and Yellow Brick Road Colebee serves an important role in achieving this role for the area. Jay is dedicated to helping families, be there with them and look out for them for the long term. "
+								</p>	
+								<p class="signature">
+									<img src="images/mark_signature.png">
+								</p>
+							</div>
+							<div class="rightside">
+								<img src="images/mark.png">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="footer_container">
+						<div class="container">
+							<ul>
+								<li>
+									<a href="#">Privacy</a>
+								</li>
+								<li>
+									<a href="#">Disclosure</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+		<script src="js/jquery.matchHeight.js"></script>
+		<script type="text/javascript">
+			$(function() {
+			    $('.rate_smasher_container').matchHeight();
+			});
+		</script>
+
+	</body>
+</html>
